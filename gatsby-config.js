4c6848613d854736f199b4822aa0d9fe38c52a26
andy <andyst81@users.noreply.github.com>
2020-11-16 22:39:44 +0700

    edit icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
 				background_color: '#2980b9',
 				theme_color: '#2980b9',
 				display: 'standalone',
-				icon: 'src/images/gatsby-icon.png',
+				icon: 'src/images/icon.png',
 			}
 		},
 		`gatsby-plugin-sass`,
